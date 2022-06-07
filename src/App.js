9db978946762f33dfba3fe7e33762fc85d03ca8f
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-06-07 14:16:26 +0100

    Initial commit, added different components to create initial app
    Had to redo because other repository was not working properly

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          {" "}
+          <h1>Dictionary</h1>
+        </header>
+        <Dictionary />
+      </div>
+      <footer className="App-footer">
+        <small>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/joannemukoko"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Joanne Mukoko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/joannemukoko/dictionary-app"
+            targt="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </small>
+      </footer>
+    </div>
+  );
+}
+export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+import Results from "./Results";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
+    // console.log(response.data[0].meanings[0].definitions[0].definition)
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    //documentation = https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
+}
