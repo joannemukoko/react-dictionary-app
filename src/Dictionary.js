bd905914cc1e7543bab927d94cb814b413752190
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-06-09 16:54:16 +0100

    Edited the css and changed the colours

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -38,8 +38,13 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h2>What word are you like to search?</h2>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className="hint">
             suggested words: love, happiness, serendipity, peace...
